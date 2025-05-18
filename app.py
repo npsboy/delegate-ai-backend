8587d533724ae628ff82ca14065e2ced17aafabf
Tushar V <tushar.vijayanagar@gmail.com>
2025-05-18 12:30:35 +0530

    removed app.run

--- app.py
+++ app.py
@@ -6,7 +6,4 @@ app = Flask(__name__)
 def index():
     return jsonify({"message": "Hello Railway!"})
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+
